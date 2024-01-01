04fc86fa3f661226b965ea328f77eb8c47c6e3b0
Amit-kumar99 <amit.kumar.16208932@gmail.com>
2024-01-02 03:19:17 +0530

    Added tailwind, made basic structure of projects section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h2>Hi, I'm Amit Kumar</h2>
+      <h2 className="">Hi, I'm Amit Kumar</h2>
       <h3>React Developer</h3>
     </div>
   )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,20 +1,43 @@
 const Projects = () => {
+  const projects=[
+    {
+      id: 1,
+      heading: "Swiggy Clone",
+      description: "",
+      link: "",
+      code: ""
+    },
+    {
+      id: 2,
+      heading: "Netflix Clone",
+      description: "...",
+      link: "",
+      code: ""
+    }
+  ];
+
   return (
-    <div>
-      <h2>Projects</h2>
+    <div className="mt-10">
+      <h2 className="font-bold text-4xl text-center">Projects</h2>
       {/* carousel */}
-      <div>
-        <div>
-          <img src="" alt="project-image"/>
-        </div>
-        <div>
-          <h3>Project name</h3>
-          <p>Project description</p>
-          <div>
-            <button>Live Link</button>
-            <button>Github Link</button>
+      <div className="flex mx-auto mt-10 w-8/12">
+        {projects.map((proj) => (
+          <div key={proj.id} className="flex">
+            <div>
+              <img src="" alt="project-image"/>
+            </div>
+            <div>
+              <div>
+                <h3 className="font-semibold text-lg">{proj.heading}</h3>
+                <p>Project description</p>
+                <div className="flex">
+                  <button className="border rounded-md p-3 my-2 mr-2">Live Link</button>
+                  <button className="border rounded-md p-3 my-2 mr-2">Github: Source Code</button>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   )
