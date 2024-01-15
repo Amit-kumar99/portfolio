e66547f41f06ee271a513bfb98d186e21d714728
Amit-kumar99 <amit.kumar.16208932@gmail.com>
2024-01-15 22:44:39 +0530

    modified Projects section, multple image automatic carousel implemented for each project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
-import project1_img from "../images/project1_ss.png";
-import project2_img from "../images/project2_ss.png";
+import project1_img1 from "../images/swiggy-1.png";
+import project1_img2 from "../images/swiggy-2.png";
+import project2_img1 from "../images/yt-1.png";
+import project2_img2 from "../images/yt-2.png";
+import project3_img1 from "../images/netflix-1.png";
 import { Link } from "react-router-dom";
 
 const Projects = () => {
@@ -10,65 +13,69 @@ const Projects = () => {
     {
       id: 1,
       heading: "Swiggy Clone",
-      description: "A food delivery application",
       tech_stack: "Html, Tailwind CSS, React",
-      img: project1_img,
+      img: [project1_img1, project1_img2],
       code: "https://github.com/Amit-kumar99/swiggy-clone"
     },
     {
       id: 2,
-      heading: "Netflix Clone",
-      description: "An OTT platform",
+      heading: "Youtube Clone",
       tech_stack: "Html, Tailwind CSS, React",
-      img: project2_img,
-      code: ""
-    }
+      img: [project2_img1, project2_img2],
+      code: "https://github.com/Amit-kumar99/youtube-clone"
+    },
+    {
+      id: 2,
+      heading: "Netflix Clone",
+      tech_stack: "Html, Tailwind CSS, React",
+      img: [project3_img1, project2_img2],
+      code: "https://github.com/Amit-kumar99/netflix-clone"
+    },
   ];
 
-  const handlePreviousClick = () => {
-    setActiveIndex(activeIndex===0 ? projects.length-1 : activeIndex-1)
-  }
-  const handleNextClick = () => {
-    setActiveIndex(activeIndex===projects.length-1 ? 0 : activeIndex+1)
+  const handleNextImageClick = () => {
+    setActiveIndex(activeIndex===projects[0].img.length-1 ? 0 : activeIndex+1)
   }
 
   useEffect(() => {
-    setTimeout(() => {
-      handleNextClick();
-    },3000);
+    const timer = setTimeout(() => {
+      handleNextImageClick();
+    },2000);
+    return () => clearTimeout(timer);
   });
 
   return (
-    <div className="mt-10 w-9/12 mx-auto">
-      <h2 className="font-bold text-5xl text-center">Projects</h2>
-        <div className="flex mt-10 mx-auto">
-          <button className=" mr-16 p-3" onClick={handlePreviousClick}>
-            <i className="fa-solid fa-less-than fa-2xl" style={{"color": "#311f51"}}></i>
-          </button>
-          <div className="mr-20">
-            <img className="w-[700px] h-[400px]" src={projects[activeIndex].img} alt="project-image"/>
-          </div>
-          <div>
-            <div>
-              <h3 className="font-semibold text-2xl mb-2">{projects[activeIndex].heading}</h3>
-              <p>{projects[activeIndex].description}</p>
-              <p>TECH STACK: <span className="font-semibold">{projects[activeIndex].tech_stack}</span></p>
-              <div className="flex mt-2">
-                <Link 
-                  className="border rounded-md p-3 my-2 mr-2 bg-violet-500 text-white"
-                  to="">Live Demo
-                </Link>
-                <Link 
-                  className="border rounded-md p-3 my-2 mr-2 bg-violet-500 text-white text-sm"
-                  to={projects[activeIndex].code}>Source Code 
-                </Link>
+    <div className="mt-10 w-11/12 mx-auto">
+      <h2 className="font-bold text-3xl">Projects</h2>
+      <div className="flex">
+        {projects.map((project) => (
+          <div key={project.id} className="flex mt-10 mx-auto">
+            <div className="border">
+              <div className="flex">
+                <div>
+                  <img className="w-[440px] h-[300px]" src={project.img[activeIndex]} alt="project-image"/>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <h3 className="font-semibold text-2xl mb-2">{project.heading}</h3>
+                  <p>TECH STACK: <span className="font-semibold">{project.tech_stack}</span></p>
+                  <div className="flex mt-2">
+                    <Link 
+                      className="border rounded-md p-3 my-2 mr-2 bg-violet-500 text-white"
+                      to="">Live Demo
+                    </Link>
+                    <Link 
+                      className="border rounded-md p-3 my-2 bg-violet-500 text-white text-sm"
+                      to={project.code}>Source Code 
+                    </Link>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <button className="ml-16 p-3" onClick={handleNextClick}>
-            <i className="fa-solid fa-greater-than fa-2xl"></i>
-          </button>
-        </div>
+        ))}
+      </div>
     </div>
   )
 }
